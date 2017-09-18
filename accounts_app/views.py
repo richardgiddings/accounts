944596b8081aa01e75903331ebee792510a8f4bc
Richard Giddings <mail@richardgiddings.co.uk>
2017-09-18 16:14:09 +0100

    Ordering transactions with newset first

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -16,6 +16,7 @@ def view_account(request, account_number):
 
     transactions = Transaction.objects.filter(Q(from_account=account_number) | 
                                               Q(to_account=account_number))
+    transactions = transactions.order_by('-datetime_of_transaction')
 
     return render(request, template_name='accounts_app/account.html',
                   context={ 'account': account, 'transactions': transactions })
