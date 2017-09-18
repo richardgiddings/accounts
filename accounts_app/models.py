57500a7fbc7e0088ad9d26ab31421554825c2ece
Richard Giddings <mail@richardgiddings.co.uk>
2017-09-18 14:39:02 +0100

    Add a transaction model

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 class Account(models.Model):
 
-    name = models.IntegerField()
+    name = models.IntegerField(primary_key=True)
     balance = models.DecimalField(max_digits=8, decimal_places=2, default=200)
     email = models.EmailField()
 
     def __str__(self):
         return str(self.name)
+
+class Transaction(models.Model):
+
+    datetime_of_transaction = models.DateTimeField(auto_now_add=True, blank=True)
+    from_account = models.IntegerField()
+    to_account = models.IntegerField()
+    amount_transferred = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return str(datetime_of_transaction)
